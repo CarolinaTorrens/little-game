aaa1eb888d9abc1a74e5d99eef6e029867725606
carolina torrens <torrenscarolina36@gmail.com>
2025-10-25 20:47:57 -0400

    add style.css

--- script.js
+++ script.js
@@ -6,9 +6,87 @@ button.addEventListener("click",()=>{
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 520;
-canvas.height = 440;
+canvas.width = 620;
+canvas.height = 540;
 canvas.style.background = "#000000ff";
+
+//INIT PAGE --------------------------------------------------------
+
+//ctx.strokeStyle = "white";
+//ctx.strokeRect(270,250,100,30);// EJE X, EJE Y, WIDTH, HEIGHT
+//ctx.textAlign("start")
+
+const buttonStart = {
+    x: canvas.width / 2 - 75, //75 es la mitad del largo del button
+    y: canvas.height / 2 - 25,
+    width: 150,
+    height: 50,
+    text: 'START',
+    color: 'white',
+    font: '24px Arial'
+};
+
+function drawButton(){
+    //dibuja el rectangulo
+    ctx.strokeStyle = buttonStart.color;
+    ctx.strokeRect(buttonStart.x, buttonStart.y, buttonStart.width, buttonStart.height);
+
+    //dibuja texto "START"
+    ctx.fillStyle = 'white';
+    ctx.font = buttonStart.font;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle'; 
+
+    //dibuja el texto en el centro del rectangulo
+    ctx.fillText(
+        buttonStart.text,
+        buttonStart.x + buttonStart.width / 2, //esto agarra la posicion x del button y le suma la mitad del  size del ancho del cuadrado 
+        buttonStart.y + buttonStart.height / 2,
+    );    
+}
+
+
+
+function drawStartScreen() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpia el canvas
+    // Opcional: Dibuja el título del juego aquí...
+   
+    
+    drawButton();
+}
+
+drawStartScreen();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 //Dibujar rectangulo blanco
 ctx.fillStyle = "white";
@@ -26,6 +104,7 @@ ctx.fillRect(200,380,100,15);// EJE X, EJE Y, WIDTH, HEIGHT
     ctx.lineTo(100, 25);//EJE X , EJE Y
     ctx.fill();
 
+    */
 
 
    
